e83d0894f5b5e6ed6a16937adc55b6746efa3810
KhoaBun <128281841+khoa231@users.noreply.github.com>
2025-06-17 08:22:01 +0700

    them chuc nang payment bang qrCode

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 from database.db import db
 from routes.api_route import api_bp
-from routes.view_route import view_bp  # Thêm dòng này
+from routes.view_route import view_bp
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///product.db'
-
-
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+@app.route('/pay')
+def pay():
+    return render_template('pay.html')
+
 app.register_blueprint(api_bp, url_prefix='/api')
-app.register_blueprint(view_bp)  # Thêm dòng này
+app.register_blueprint(view_bp)
 
 db.init_app(app)
 
